7ab5163863a7e24e40ea4223024780f5587b5d21
JS2IIU-MH <makoto.harazawa@gmail.com>
2023-10-18 23:14:08 +0900

    small update

--- tkinter/tkinter_treeview_template.py
+++ tkinter/tkinter_treeview_template.py
@@ -1,6 +1,7 @@
 '''tkinter GUI sample
     open window with label, button and entry widgets.
 '''
+import os
 
 import tkinter as tk
 from tkinter import ttk
@@ -23,34 +24,40 @@ class Application(tk.Frame):
         super().__init__(master)
         self.grid()
 
-        self.treeview_from_csv(master)
-
         frame1 = tk.Frame(master)
-
         label1 = tk.Label(frame1,
                           text='File: ',
                           )
         label1.grid(row=0, column=0)
-
         label2 = tk.Label(frame1,
-                          width=15,
+                          width=25,
                           text='',
                           justify=tk.CENTER,
                           border=1,
                           )
         label2.grid(row=0, column=1)
+        frame1.grid()
 
-        extbtn = tk.Button(frame1,
+        frame2 = tk.Frame(master)
+        self.treeview_from_csv(frame2)
+        frame2.grid()
+
+        frame3 = tk.Frame(master)
+        extbtn = tk.Button(frame3,
                            text='Exit',
                            command=quit,
                            )
         extbtn.grid(row=0, column=2)
-        frame1.grid()
+        frame3.grid()
 
     def treeview_from_csv(self, master):
         ''' treeview_from_csv '''
+        sample_csv = 'treeview_data.csv'
+        path, _ = os.path.split(__file__)
+        sample_file_path = os.path.join(path, sample_csv)
+
         # read_csv
-        _df = pd.read_csv('tkinter/treeview_data.csv', comment='#')
+        _df = pd.read_csv(sample_file_path, comment='#')
         column_list = _df.columns.to_list()
 
         # treeview
@@ -59,17 +66,14 @@ class Application(tk.Frame):
                                  selectmode=tk.EXTENDED,
                                  height=5)
 
-        # column setting
+        # column setting, heading
         self.tree.column('#0', width=0, stretch=tk.NO)
+        self.tree.heading('#0', text='')
         for _c in column_list:
             if _c == 'ID':
                 self.tree.column(_c, width=25, anchor=tk.CENTER)
             else:
                 self.tree.column(_c, width=80, anchor=tk.CENTER)
-
-        # heading
-        self.tree.heading('#0', text='')
-        for _c in column_list:
             self.tree.heading(_c, text=_c, anchor=tk.CENTER)
 
         # recoed
@@ -78,6 +82,7 @@ class Application(tk.Frame):
             # print(values)
             self.tree.insert(parent='', index=tk.END, iid=i, values=values)
 
+        # scrollbar
         scrollbar = ttk.Scrollbar(master, orient=tk.VERTICAL,
                                   command=self.tree.yview)
         self.tree.configure(yscrollcommand=scrollbar.set)
